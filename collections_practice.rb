f5403e3334bb32f09aa79fe26af8adaf4d00333d
Victor Plushko <vicplushko33@gmail.com>
2020-04-02 05:31:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort { |a, b| a <=> b}
+end
 
+def sort_array_desc(array)
+  array.sort! { |a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+ array[0], array[1], array[2] = array[0], array[2], array[1]
+ end
+ 
+def reverse_array(array)
+  array.reverse {|num| num.reverse}
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |char|
+    char_array = char.split("")
+    char_array[2] = "$"
+    new_array << char_array.join("")
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject { |sum, number| sum + number}
+end
+
+def add_s(array)
+  new_array = []
+  array.each_with_index do |word, index|
+    word.insert("s")
+    index = [0, 2, 3]
+    new_aray
+end
+end
+ 
